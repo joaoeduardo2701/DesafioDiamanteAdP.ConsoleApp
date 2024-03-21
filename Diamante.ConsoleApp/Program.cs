7b8ccdba9d70f1ba3f5013ca008eb2492013ff36
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-21 13:49:11 -0300

    Add project files.

---
+++ Diamante.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Diamante.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o tamanho do diamante: ");
+        int tamanho = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            //parte de cima
+
+            for (int j = 0; j < tamanho / 2; j++)
+            {
+                Console.Write("b");
+
+            }
+
+            //parte do meio
+
+            Console.Write("x");
+
+            //parte de baixo
+        }
+    }
+}
